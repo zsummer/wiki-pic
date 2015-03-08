dd52864da0e090dd9357c270140ff3dff70e170f
zhangyawei <yawei_zhang@foxmail.com>
2015-03-08 22:09:20 +0800

    update

--- fragment/proxy.sh
+++ fragment/proxy.sh
@@ -1,5 +1,4 @@
-cd /Users/zhangyawei/.ssh/
-
+#!/bin/bash
 while :
 do
 	pid=`ps -ef|grep ssh |grep 8087|awk '{print $2}' | xargs`
@@ -7,11 +6,11 @@ do
 		kill $pid
 	fi
 	port=`netstat -na|grep 8087| xargs`
-	if [ "$port" == "" ]; then	
+	if [ "$port" = "" ]; then	
 		break
 	fi
 	sleep 1
 done
 
-
-ssh -qfTnN -iid_rsa -D127.0.0.1:8087 zhangyawei@1.1.1.1
+#ssh -qfTnN -i/Users/zhangyawei/.ssh/id_rsa -D0.0.0.0:8087 zhangyawei@107.191.153.14
+ssh -qfTnN -i/root/.ssh/zhangyawei_rsa -D0.0.0.0:8087 zhangyawei@107.191.153.14
