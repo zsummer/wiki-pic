377e90fa347012989435967b14df79f3dca67c4c
zhangyawei <yawei_zhang@foxmail.com>
2015-03-25 21:12:13 +0800

    skip when used.

--- fragment/proxy.sh
+++ fragment/proxy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-source /etc/profile
+#source /etc/profile
+port=8087
+listens=`netstat -na|grep "\b$port\b" | wc -l`
+if [ $listens -gt 1 ]; then
+#	echo "in used"
+	exit
+fi
 
 while :
 do
@@ -14,5 +20,5 @@ do
 	sleep 1
 done
 
-ssh -qfTnN -i/Users/zhangyawei/.ssh/id_rsa -D0.0.0.0:8087 zhangyawei@107.191.53.14
-#ssh -qfTnN -i/root/.ssh/zhangyawei_rsa -D0.0.0.0:8087 zhangyawei@107.191.153.14
+#ssh -qfTnN -i/Users/zhangyawei/.ssh/id_rsa -D0.0.0.0:8087 zhangyawei@107.191.153.14
+ssh -qfTnN -i/root/.ssh/zhangyawei_rsa -D0.0.0.0:8087 zhangyawei@107.191.153.14
